21b352720fc976a97998f950d09899f6242504cb
jason van pelt <jvanpelt@gmail.com>
2017-03-17 21:56:18 -0500

    disable scroll -- will it still log scrolling to the console?

--- scripts.js
+++ scripts.js
@@ -1,8 +1,29 @@
 $('document').ready(function(){
    
+   disableScroll();
+   
     var isScrolling = false;
     $(window).on("scroll",function(){
         isScrolling = true;
         console.log("shit is scrolling!!");
     });
 });
+
+
+function disableScroll() {
+    if (window.addEventListener){ // older FF
+        window.addEventListener('DOMMouseScroll', preventDefault, false);
+    }
+    window.onwheel = preventDefault; // modern standard
+    window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+    window.ontouchmove  = preventDefault; // mobile
+    document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault){
+        e.preventDefault();
+    }
+    e.returnValue = false;  
+}
