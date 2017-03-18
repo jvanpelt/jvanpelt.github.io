75b2c830c28f97079fa0a69b5bfd665678de015d
jason van pelt <jvanpelt@gmail.com>
2017-03-17 21:42:48 -0500

    then entered javascript...

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
 // Your scripts go here.
 
 console.log('hello world!');
+
+var isScrolling = false;
+$(window).on("scroll",function(){
+    isScrolling = true;
+    console.log("shit is scrolling!!");
+});
